a2d7c7930e40d9d1fef72ae0da18daea7783ae4a
machenxing <machenxing@139.com>
2018-11-29 15:10:21 +0800

    十进制与十六进制互相转换

---
+++ src/NumChange.java
@@ -0,0 +1,11 @@
+/**
+ 十进制与十六进制互相转换
+ */
+public class NumChange {
+    public static void main(String[] args) {
+        // 十进制转换为十六进制
+        System.out.println(Integer.toHexString(916722));
+        // 十六进制转换为十进制
+        System.out.println(Integer.parseInt("7de7f",16));
+    }
+}
