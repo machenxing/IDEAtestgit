c77df52216b59b82a215de25fdb7b3c7dc712f1a
machenxing <machenxing@139.com>
2019-03-10 22:47:12 +0800

    Java8 filter过滤器

---
+++ src/Filter.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Java8 filter过滤器
+ */
+public class Filter {
+    public static void main(String[] args) {
+        List<Long> listLong = new ArrayList<>();
+        for(int i=0; i<100000 ; i++) {
+            listLong.add( (long) i);
+        }
+        List<Long> stream = listLong.stream().filter(Long ->{
+            return Long.equals(666L) || Long.equals(777L) || Long.equals(888L);
+        }).collect(Collectors.toList());
+        for (Long l : stream) {
+            System.out.println(l);
+        }
+    }
+}
